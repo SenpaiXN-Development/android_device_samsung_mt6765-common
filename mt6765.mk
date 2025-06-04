541a18a22166e6fcbcc95a77b1a50f95b7b02336
jmpfbmx <jmpf.bmx@gmail.com>
2025-06-05 01:18:59 +0200

    mt6765-common: Build Media stack
    Change-Id: Ie1372e565f259e047ad416707bae156f24224a7d
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- mt6765.mk
+++ mt6765.mk
@@ -181,6 +181,14 @@ PRODUCT_PACKAGES += \
     android.hardware.light-service.mt6765-common
 
 # Media
+PRODUCT_PACKAGES += \
+    libcodec2_hidl@1.1.vendor \
+    libcodec2_hidl@1.2.vendor \
+    libavservices_minijail_vendor \
+    libstagefright_softomx_plugin.vendor \
+    libsfplugin_ccodec_utils.vendor \
+    libcodec2_soft_common.vendor
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/media/media_codecs_c2.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_c2.xml \
     $(LOCAL_PATH)/media/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
