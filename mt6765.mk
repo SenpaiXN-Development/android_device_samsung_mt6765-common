91d27e054c3c83fc21958d2b3788eb0508a59beb
Woomymy <woomy@woomy.be>
2025-07-17 15:57:14 +0200

    mt6765-common: Add dummy libpowerhalwrap_vendor library
    * Required by some vendor blobs that depend on it
    
    Signed-off-by: Woomymy <woomy@woomy.be>
    Change-Id: I6d0b16800a372cb0094fc4cf910b01d3fe05a37e

--- mt6765.mk
+++ mt6765.mk
@@ -263,7 +263,8 @@ PRODUCT_PACKAGES += \
     vendor.mediatek.hardware.mtkpower@1.1.vendor \
     vendor.mediatek.hardware.mtkpower@1.2-service.stub \
     libmtkperf_client_vendor \
-    libmtkperf_client
+    libmtkperf_client \
+    libpowerhalwrap_vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
