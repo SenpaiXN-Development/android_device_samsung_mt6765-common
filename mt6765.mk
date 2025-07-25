40500fa5898e32d83875eeab2f6803cd6a975952
TheMalachite <eliasgheeraert@gmail.com>
2025-07-25 23:16:15 +0200

    mt6765-common: Add persist.log.tag properties overrides
    Change-Id: Ibd6a89cbc0fa17b36fff1a0759afb64705687d13

--- mt6765.mk
+++ mt6765.mk
@@ -214,6 +214,9 @@ PRODUCT_PACKAGES += \
     libsysutils.vendor \
     libunwindstack.vendor
 
+# Properties
+include $(LOCAL_PATH)/vendor_logtag.mk
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
