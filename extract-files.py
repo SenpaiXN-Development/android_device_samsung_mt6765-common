cd06bcec0f65488584d0e9ec6b70871ae4be8cdd
jmpfbmx <jmpf.bmx@gmail.com>
2025-07-25 21:38:43 +0200

    mt6765-common: Link Camera hal against older lib{hidlbase, utils}
    Change-Id: I6afde9558aa2819019dfe272be502f9b9b847621
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -55,6 +55,11 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
         .replace_needed('libutils.so', 'libutils-v32.so'),
     (
+        'vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so',
+    ): blob_fixup()
+        .replace_needed('libhidlbase.so', 'libhidlbase-v32.so')
+        .replace_needed('libutils.so', 'libutils-v32.so'),
+    (
         'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
         'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b',
     ): blob_fixup()
