ef275470225590baa52b49a4b803ab04f5d5cc01
bengris32 <bengris32@protonmail.ch>
2025-07-25 21:37:42 +0200

    mt6765-common: Patchelf C2 service to link against libavservices_minijail
    * The libavservices_minijail_vendor library was substituted for
      the vendor variant of libavservices_minijail in Android 13.
    
    * Patchelf the C2 service to link against libavservices_minijail
      instead of libavservices_minijail_vendor.
    
    Change-Id: I4cddc98e91724c86e7761fee41d4f9f5554c7203
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- extract-files.py
+++ extract-files.py
@@ -58,7 +58,8 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek',
         'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b',
     ): blob_fixup()
-        .add_needed('libstagefright_foundation-v33.so'),
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
     (
         'vendor/bin/mnld',
         'vendor/lib64/libcam.utils.sensorprovider.so',
