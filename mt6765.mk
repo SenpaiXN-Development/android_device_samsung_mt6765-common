69d17a47aabfa7b5b58b207d0b315df4088a5706
ZaraKinYu <chikushinemathew@gmail.com>
2025-10-02 16:56:02 +0530

    mt6765-common: build OSS secril_config_svc

--- mt6765.mk
+++ mt6765.mk
@@ -289,7 +289,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.config@1.3.vendor \
-    android.hardware.radio.deprecated@1.0.vendor
+    android.hardware.radio.deprecated@1.0.vendor \ 
+    secril_config_svc
 endif
 
 # Rootdir
